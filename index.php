75da6082a4ab817286c24eb54b42d4d3cd1dd6f4
Darren Gideon <136311989+Cupcake-Legend@users.noreply.github.com>
2024-09-03 09:10:55 +0700

    Switch case statements, basic routing

--- index.php
+++ index.php
@@ -6,12 +6,34 @@ require_once 'controllers/MemberController.php';
 // Database connection setup (replace with your own connection details)
 $db = new mysqli('localhost', 'root', '', 'esport_fullstack');
 
-$memberModel = new memberModel($db);
-$memberController = new memberController($accountModel);
+$memberModel = new MemberModel($db);
+$memberController = new MemberController($memberModel);
 
-// Basic routing
-if ($_SERVER['REQUEST_URI'] == '/account/add') {
-    $memberController->addAccount();
-} else {
-    require_once 'views/home.php';
+// Get the request URI, trim it, and split it into parts
+$requestUri = trim($_SERVER['REQUEST_URI'], '/');
+$uriSegments = explode('/', $requestUri);
+
+// Basic routing using switch case
+switch ($uriSegments[0]) {
+    case 'member':
+        if (isset($uriSegments[1])) {
+            switch ($uriSegments[1]) {
+                case 'add':
+                    $memberController->addAccount();
+                    break;
+                // Add more cases here for other member-related actions
+                default:
+                    require_once 'views/member_home.php';
+                    break;
+            }
+        } else {
+            require_once 'views/member_home.php';
+        }
+        break;
+
+    // Add more cases here for other base routes like 'account', 'team', etc.
+
+    default:
+        require_once 'views/home.php';
+        break;
 }
