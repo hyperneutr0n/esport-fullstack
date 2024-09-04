ddf71deca2c99f25f376087944269ffd5503bdca
himeriusnico <himeriusnico03@gmail.com>
2024-09-04 12:00:17 +0700

    Create game model and game controller
    finished all CRUD and needed
    
    (-) Beberapa page ta direct tpi blm create page view nya

---
+++ controllers/GameController.php
@@ -0,0 +1,71 @@
+<?php
+class GameController
+{
+  private $model;
+
+  public function __construct($model)
+  {
+    $this->model = $model;
+  }
+
+  public function addGame()
+  {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["add"])) {
+      $name = $_POST["name"];
+      $description = $_POST["description"];
+
+      if ($this->model->addGame($name, $description)) {
+        echo "Game Added Successfully"; //sementara 
+      } else {
+        echo "Failed to add game.";
+      }
+    }
+  }
+
+  public function deleteGame($idgame)
+  {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["delete"])) {
+      if ($this->model->deleteGame($idgame)) {
+        echo "Game Deleted";
+      } else {
+        echo "Failed to delete game.";
+      }
+    } else {
+      echo "Game not found";
+    }
+  }
+
+  public function editGame($idgame)
+  {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["edit"])) {
+      $name = $_POST["name"];
+      $description = $_POST["description"];
+
+      if ($this->model->updateGame($idgame, $name, $description)) {
+        echo "Edit game success.";
+      } else {
+        echo "Failed to edit game.";
+      }
+    } else {
+      echo "Game not found";
+    }
+  }
+
+  public function showAddGameForm()
+  {
+    require_once 'views/add_game.php';
+
+    //page ta buat gini ?
+  }
+
+  public function showEditGameForm($idgame)
+  {
+    $game = $this->model->getGameById($idgame);
+    if ($game) {
+      require_once 'views/edit_game.php';
+      // ini juga 
+    } else {
+      echo "Game not found.";
+    }
+  }
+}
---
+++ models/Game.php
@@ -0,0 +1,64 @@
+<?php
+
+class Game
+{
+  private $db;
+
+  public function __construct($database)
+  {
+    $this->db = $database;
+  }
+
+  public function addGame($name, $description)
+  {
+    $sql =  "INSERT INTO game (name, description) VALUES (?, ?)";
+    $stmt = $this->db->prepare($sql);
+    $stmt->bind_param("ss", $name, $description);
+
+    if ($stmt->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function deleteGame($idgame)
+  {
+    $sql = "DELETE FROM game WHERE idgame = ?";
+    $stmt = $this->db->prepare($sql);
+    $stmt->bind_param("i", $idgame);
+
+    if ($stmt->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function updateGame($idgame, $name, $description)
+  {
+    $sql = "UPDATE game SET name = ?, description = ?, WHERE idgame =?";
+    $stmt = $this->db->preaper($sql);
+    $stmt->bind_param("ssi", $name, $description, $idgame);
+
+    if ($stmt->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function selectById($idgame)
+  {
+    $sql = "SELECT * FROM game WHERE idgame = ?";
+    $stmt = $this->db->prepare($sql);
+    $stmt->bind_param("i", $idgame);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+      return $result->fetch_assoc();
+    } else {
+      return null;
+    }
+  }
+}
