ae664c9b13e7ab2befc40f1acce7cc9b24733256
hyperneutr0n <randylemuel21@gmail.com>
2024-10-09 07:21:01 +0700

    Minor changes on detail_team

--- views/admin/details/detail_team.php
+++ views/admin/details/detail_team.php
@@ -55,31 +55,31 @@
         </tr>
       <?php } ?>
     </tbody>
-    <h2>These are this team's achievements</h2>
-    <table border="1">
-      <thead>
+  </table>
+  <h2>These are this team's achievements</h2>
+  <table border="1">
+    <thead>
+      <tr>
+        <th>ID Achievement</th>
+        <th>Name</th>
+        <th>Date</th>
+        <th>Description</th>
+        <th colspan=" 2">Aksi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($achievements as $achievement) { ?>
+        <?php $id = $achievement["idachievement"]; ?>
         <tr>
-          <th>ID Achievement</th>
-          <th>Name</th>
-          <th>Date</th>
-          <th>Description</th>
-          <th colspan=" 2">Aksi</th>
+          <td><?= $achievement["idachievement"] ?></td>
+          <td><?= $achievement["name"] ?></td>
+          <td><?= $achievement["date"] ?></td>
+          <td><?= $achievement["description"] ?></td>
+          <td><a href="/admin/updateachievement?id=<?= $id ?>" class="action-link blue-link">Update</a></td>
+          <td><a href="/process/deleteachievement?id=<?= $id ?>" class="action-link red-link">Delete</a></td>
         </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($achievements as $achievement) { ?>
-          <?php $id = $achievement["idachievement"]; ?>
-          <tr>
-            <td><?= $achievement["idachievement"] ?></td>
-            <td><?= $achievement["name"] ?></td>
-            <td><?= $achievement["date"] ?></td>
-            <td><?= $achievement["description"] ?></td>
-            <td><a href="/admin/updateachievement?id=<?= $id ?>" class="action-link blue-link">Update</a></td>
-            <td><a href="/process/deleteachievement?id=<?= $id ?>" class="action-link red-link">Delete</a></td>
-          </tr>
-        <?php } ?>
-      </tbody>
-    </table>
+      <?php } ?>
+    </tbody>
   </table>
 
 </div>
